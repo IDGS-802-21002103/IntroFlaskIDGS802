093b6d36dd56c92eea9ed5be07aa04154a740ecf
Damian <johndoe@example.com>
2024-01-29 22:53:42 -0600

    FDGL 29/01/2024 Formularios

--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
 '''Modulo para la creacion de formularios'''
-from wtforms import Form, RadioField, StringField, validators, EmailField, SelectField
+from wtforms import Form
+from wtforms import RadioField
+from wtforms import StringField
+from wtforms import EmailField
+from wtforms import SelectField
 
 class  UserForm(Form):
     '''Formulario para el registro de usuarios'''
-    nombre = StringField('Nombre', [validators.Length(min=1, max=50)])
-    apellido = StringField('Apellido', [validators.Length(min=1, max=50)])
-    email = EmailField('Email', [validators.Length(min=6, max=50)])
+    nombre = StringField('Nombre')
+    apellido = StringField('Apellido')
+    email = EmailField('Email')
     materias = SelectField(choices=[('Espanol','Esp'), ('Matematicas','Mat'), ('Ingles','Ing')])
-    radios = RadioField('Curso', choices=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5'), ('6','6')])
+    radios = RadioField('Curso', choices=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5')])
--- main.py
+++ main.py
@@ -1,56 +1,82 @@
-'''Archivo principal de la aplicacion'''
+"""Archivo principal de la aplicacion"""
+
 from flask import Flask, request, render_template
+
+import forms
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    '''Funcion inicial de la aplicacion'''
+    """Funcion inicial de la aplicacion"""
     return render_template("index.html")
 
-@app.route('/maestros')
+
+@app.route("/maestros")
 def maestros():
-    '''Funcion para mostrar los maestros'''
+    """Funcion para mostrar los maestros"""
     return render_template("maestros.html")
 
-@app.route('/alumnos')
+
+@app.route("/alumnos", methods=["GET", "POST"])
 def alumnos():
-    '''Funcion para mostrar los alumnos'''
-    return render_template("alumnos.html")
+    """Funcion para mostrar los alumnos"""
+    alumno_form = forms.UserForm(request.form)
+    nombre = ''
+    apellido = ''
+    email = ''
+    
+    if request.method == "POST":
+        nombre = alumno_form.nombre.data
+        apellido = alumno_form.apellido.data
+        email = alumno_form.email.data
+        
+        print("Nombre: {}".format(nombre))
+        print("Apellido: {}".format(apellido))
+        print("Email: {}".format(email))
+        
+    return render_template("alumnos.html", form=alumno_form, nombre=nombre, apellido=apellido, email=email)
 
-@app.route('/')
+
+@app.route("/hello")
 def hola_mundo():
-    '''Funcion inicial de la aplicacion'''
-    return '<p>Hello, World!<p>'
+    """Funcion inicial de la aplicacion"""
+    return "<p>Hello, World!<p>"
 
-@app.route('/hola')
+
+@app.route("/hola")
 def hola():
-    '''Funcion Hola de la aplicacion'''
-    return '<p>Hola, Mundo!<p>'
+    """Funcion Hola de la aplicacion"""
+    return "<p>Hola, Mundo!<p>"
+
 
 @app.route("/saludo/<string:nombre>")
 def saludo(nombre):
-    '''Funcion Saludo de la aplicacion'''
+    """Funcion Saludo de la aplicacion"""
     return f"<p>Hola, {nombre}!<p>"
+
 
 @app.route("/numero/<int:num>")
 def numero(num):
-    '''Funcion Numero de la aplicacion'''
+    """Funcion Numero de la aplicacion"""
     return f"<p>El numero es {num}<p>"
 
+
 @app.route("/user/<string:username>/<int:identificador>")
-def user(username,identificador):
-    '''Funcion par Usuarios'''
+def user(username, identificador):
+    """Funcion par Usuarios"""
     return f"ID: {identificador} NOMBRE: {username}"
+
 
 @app.route("/suma/<int:num1>/<int:num2>")
 def suma(num1, num2):
-    '''Funcion Suma de la aplicacion'''
+    """Funcion Suma de la aplicacion"""
     return f"La suma de {num1} + {num2} es {num1 + num2}"
+
 
 @app.route("/multiplica", methods=["GET", "POST"])
 def multiplica():
-    '''Funcion Multiplica de la aplicacion'''
+    """Funcion Multiplica de la aplicacion"""
     if request.method == "POST":
         numero_1 = request.form.get("numero1")
         numero_2 = request.form.get("numero2")
@@ -58,7 +84,7 @@ def multiplica():
         return f"<h1>El resultado es: {resultado}</h1>"
 
     else:
-        return '''
+        return """
                 <form action="/multiplica" method="POST">
                     <label>Numero 1</label>
                     <input type="text" name="numero1">
@@ -66,16 +92,18 @@ def multiplica():
                     <input type="text" name="numero2">
                     <input type="submit" value="Multiplica">
                 </form>
-                 '''
+                 """
+
 
 @app.route("/formulario1")
 def formulario():
-    '''Funcion Formulario de la aplicacion'''
+    """Funcion Formulario de la aplicacion"""
     return render_template("formulario_1.html")
+
 
 @app.route("/resultado", methods=["POST", "GET"])
 def multiplicacion():
-    '''Funcion Multiplicacion de la aplicacion'''
+    """Funcion Multiplicacion de la aplicacion"""
     if request.method == "POST":
         numero_1 = request.form.get("numero1")
         numero_2 = request.form.get("numero2")
@@ -83,5 +111,6 @@ def multiplicacion():
         return f"<h1>El resultado es: {resultado}</h1>"
     return "Error"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
