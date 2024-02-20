38adfc2bef1252e221b3855ce1fcfa9194710577
Damian <johndoe@example.com>
2024-02-19 20:49:53 -0600

    FDGL 19/02/2024 Variable Global

--- forms.py
+++ forms.py
@@ -12,6 +12,6 @@ class  UserForm(Form):
     nombre = StringField('Nombre', [validators.DataRequired(message='El campo es requerido'), validators.length(min=4, max=10, message='Ingrese un nombre valido')])
     apellido = StringField('Apellido')
     email = EmailField('Email', [validators.DataRequired(message='El campo es requerido'), validators.Email(message='Ingrese un email valido')])
-    materias = SelectField(choices=[('Espanol','Esp'), ('Matematicas','Mat'), ('Ingles','Ing')])
-    radios = RadioField('Curso', choices=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5')])
+    # materias = SelectField(choices=[('Espanol','Esp'), ('Matematicas','Mat'), ('Ingles','Ing')])
+    # radios = RadioField('Curso', choices=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5')])
     edad = StringField('Edad', [validators.DataRequired(message='El campo es requerido'), validators.length(min=1, max=2, message='Ingrese una edad valida')])
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 """Archivo principal de la aplicacion"""
 
 from flask import Flask, request, render_template
-
+from flask_wtf.csrf import CSRFProtect
+from flask import flash
+from flask import g
 import forms
 
 app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+
+@app.errorhandler(404)
+def page_not_found(error):
+        return render_template('error.html'), 404
+
+@app.before_request
+def before_request():
+    g.prueba = 'Hola'
+    print("ANTES DE LA RUTA")
+
+@app.after_request
+def after_request(response):
+    print("DESPUES DE LA RUTA")
+    return response
 
 @app.route("/")
 def index():
     """Funcion inicial de la aplicacion"""
     return render_template("index.html")
 
-
 @app.route("/maestros")
 def maestros():
     """Funcion para mostrar los maestros"""
     return render_template("maestros.html")
-
 
 @app.route("/alumnos", methods=["GET", "POST"])
 def alumnos():
@@ -25,18 +40,26 @@ def alumnos():
     nombre = ''
     apellido = ''
     email = ''
+    valor = g.prueba
+    print("EL valor es", valor)
+    print(request.method)
+    print("Formulario Valido",alumno_form.validate())
+    print(alumno_form.errors)
     
     if request.method == "POST" and alumno_form.validate():
         nombre = alumno_form.nombre.data
         apellido = alumno_form.apellido.data
         email = alumno_form.email.data
+        edad = alumno_form.edad.data
+        flash("Alumno registrado correctamente")
         
         print("Nombre: {}".format(nombre))
         print("Apellido: {}".format(apellido))
         print("Email: {}".format(email))
         
-    return render_template("alumnos.html", form=alumno_form, nombre=nombre, apellido=apellido, email=email)
-
+        return render_template("alumnos.html", form=alumno_form, nombre=nombre, apellido=apellido, email=email, edad=edad)
+    
+    return render_template("alumnos.html", form=alumno_form, nombre='', apellido='', email='', edad='')
 
 @app.route("/hello")
 def hola_mundo():
@@ -94,7 +117,6 @@ def multiplica():
                 </form>
                  """
 
-
 @app.route("/formulario1")
 def formulario():
     """Funcion Formulario de la aplicacion"""
@@ -110,7 +132,6 @@ def multiplicacion():
         resultado = int(numero_1) * int(numero_2)
         return f"<h1>El resultado es: {resultado}</h1>"
     return "Error"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
